d99993a382549b77e1e037d4c73e1ef780c321bf
MEISSONNIER Kévin <k.meissonnier@forprodis.fr>
2016-03-18 10:12:51 +0100

    Initialisation

---
+++ dumpman.py
@@ -0,0 +1,24 @@
+import sublime, sublime_plugin, re
+
+class DumpmanCommand(sublime_plugin.TextCommand):
+	def run(self, edit):
+		for region in self.view.sel():
+			if region.empty():
+				line = self.view.line(region)
+				content = "dd(" + self.view.substr(line) + ");\n"
+				self.view.insert(edit, line.end() + 1, content)
+
+			else:
+				selection = self.view.substr(region)
+
+				content = "dd(" + selection + ");\n"
+				line = self.view.line(region)
+				lineStr = self.view.substr(line)
+				indentation = lineStr.replace(lineStr.lstrip(),'')
+
+				self.view.insert(edit, line.end() + 1, indentation + content)
+
+
+
+
+
